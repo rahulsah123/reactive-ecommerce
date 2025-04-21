8041f19fe9ae79c65a3c32949d11797cdb24aadb
rahulsah123 <smartrahulsah17@gmail.com>
2025-04-21 20:26:11 +0530

    Added discovery service

---
+++ discovery-service/src/main/java/com/example/reactive_ecommerce/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.reactive_ecommerce;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
